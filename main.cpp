3d11638254a3d3c4d474e584c33e5ef25fa1bd61
kushal <kushalkashyap.ai@gmail.com>
2026-06-16 08:05:58 +0000

    Initial commit - project setup

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <thread>
+using namespace std;
+
+int main(){
+	cout << "RTOS Simulator - Environment Ready!" << endl;
+	cout << "C++ Version: " << __cplusplus << endl;
+}	
